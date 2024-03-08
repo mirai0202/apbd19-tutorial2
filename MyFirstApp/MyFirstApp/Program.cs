e14c84fd6bf49fbeb8af87e25bd7c0a50ff0cb49
Artem Buturlakin <artem@buturlakin.family>
2024-03-08 18:48:35 +0100

    Feature average

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    
+    foreach (var number in numbers)
+        sum += number;
+    
+    return sum / numbers.Length;
+}
+
+int[] numbers = {1, 2, 3, 4};
+double average = GetAverage(numbers);
+Console.Write(average);
